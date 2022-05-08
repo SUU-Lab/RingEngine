910b613a23d8146408284d9542df5f35020a4c12
suudai <34059792+n-suudai@users.noreply.github.com>
2022-05-08 13:30:49 +0900

    Windowのクライアント領域を表すクラスを追加

---
+++ Engine/Source/Runtime/Window/Private/ClientExtent.cpp
@@ -0,0 +1,20 @@
+#include "ClientExtent.hpp"
+
+namespace Ring {
+
+ClientExtent::ClientExtent(const std::uint32_t width, const std::uint32_t height)
+    : width_(width)
+    , height_(height)
+{}
+
+std::uint32_t ClientExtent::Width() const
+{
+    return width_;
+}
+
+std::uint32_t ClientExtent::Height() const
+{
+    return height_;
+}
+
+} // namespace Ring
