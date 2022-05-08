0064d1af2831cca8e93482b588a4e3373e68fa4a
suudai <34059792+n-suudai@users.noreply.github.com>
2022-05-08 16:56:21 +0900

    Windows用のWindowクラスを追加

--- Engine/Source/Runtime/Window/Public/Windows/WindowsWindow.hpp
+++ Engine/Source/Runtime/Window/Public/Windows/WindowsWindow.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Windows/Windows.hpp"
 #include "ClientExtent.hpp"
 #include "WindowExtent.hpp"
 #include <string_view>
@@ -10,8 +11,24 @@ class WindowsWindow
 {
 public:
     WindowsWindow(const ClientExtent& clientExtent, std::string_view title);
+    ~WindowsWindow();
 
     bool Update();
+
+    std::string GetTitle() const;
+
+    ClientExtent GetClientExtent() const;
+
+    WindowExtent GetWindowExtent() const;
+
+private:
+    static LRESULT CALLBACK WndProc(
+        HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam
+    );
+
+    void Destroy();
+
+    HWND m_hWnd = nullptr;
 };
 
 typedef WindowsWindow Window;
---
+++ Engine/Source/Runtime/Window/Tests/Test_Window.cpp
@@ -0,0 +1,17 @@
+#include "Window.hpp"
+#include <gtest/gtest.h>
+
+TEST(Window, Property)
+{
+    const Ring::Window window({ 640, 480 }, "Test_Window");
+
+    EXPECT_EQ("Test_Window", window.GetTitle());
+
+    const Ring::ClientExtent clientExtent = window.GetClientExtent();
+    EXPECT_EQ(640, clientExtent.Width());
+    EXPECT_EQ(480, clientExtent.Height());
+
+    const Ring::WindowExtent windowExtent = window.GetWindowExtent();
+    EXPECT_LE(clientExtent.Width(), windowExtent.Width());
+    EXPECT_LE(clientExtent.Height(), windowExtent.Height());
+}
