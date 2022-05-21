dc3d4706a45de57bce9458c7c6e69a985acd9146
suudai <34059792+n-suudai@users.noreply.github.com>
2022-05-21 23:58:21 +0900

    テストの修正

--- Engine/Source/Runtime/Window/Tests/Test_ClientExtent.cpp
+++ Engine/Source/Runtime/Window/Tests/Test_ClientExtent.cpp
@@ -3,7 +3,7 @@
 
 TEST(ClientExtent, Construction)
 {
-    const ring::ClientExtent clientExtent = { 641, 480 };
+    const ring::ClientExtent clientExtent = { 640, 480 };
 
     EXPECT_EQ(640, clientExtent.Width());
     EXPECT_EQ(480, clientExtent.Height());
