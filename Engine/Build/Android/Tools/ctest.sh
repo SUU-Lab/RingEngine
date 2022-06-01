b93227a8037f4b6684f16674b5debde3bd40a841
Nishii Takahiro <n.suudai@gmail.com>
2022-06-01 18:17:54 +0900

    テストスクリプトを修正

--- Engine/Build/Android/Tools/ctest.sh
+++ Engine/Build/Android/Tools/ctest.sh
@@ -3,27 +3,20 @@
 DIRNAME=$(dirname "$0")
 CURRENT=$(cd "$DIRNAME" || exit 1;pwd)
 
-pushd "$CURRENT/../$1" > test_log.txt || exit 1
+pushd "$CURRENT/../$1" > "$CURRENT/test_log.txt" || exit 1
 
 cleanup() {
-    popd >> test_log.txt || exit 1
+    popd >> "$CURRENT/test_log.txt" || exit 1
 }
 
-# read cmake_binary_dir.txt
-BUILD_DIR=""
-while read -r LINE
-do
-    BUILD_DIR="$BUILD_DIR$LINE"
-done < cmake_binary_dir.txt
+BUILD_DIR=$(cat cmake_binary_dir.txt)
 
-echo BUILD_DIR="$BUILD_DIR" >> test_log.txt
-
-pushd "$BUILD_DIR" >> test_log.txt || exit 1
+pushd "$BUILD_DIR" >> "$CURRENT/test_log.txt" || exit 1
 
 shift
 ctest "$@"
 
-popd >> test_log.txt || exit 1
+popd >> "$CURRENT/test_log.txt" || exit 1
 
 cleanup || exit 1
 
