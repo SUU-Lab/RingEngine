725d6c8a9326abd18b9c22d01138a9c2e93f107e
suudai <34059792+n-suudai@users.noreply.github.com>
2022-07-23 19:28:03 +0900

    Modulesのテストを追加

---
+++ Engine/Source/Runtime/Modules/Tests/Test_ModuleEntries.cpp
@@ -0,0 +1,31 @@
+﻿#include <gtest/gtest.h>
+#include "ModuleEntries.hpp"
+#include <string>
+
+TEST(ModuleEntries, Construction)
+{
+    using namespace ring;
+
+    ModuleEntries moduleEntries;
+
+    std::string name = "Name";
+
+    moduleEntries.Entry(
+        ModuleEntryPoint::Core_Initialize,
+        [&]() {
+            name = "Core_Initialize";
+        });
+    EXPECT_EQ(name, "Name");
+
+    // 同じエントリに重複できない
+    EXPECT_DEATH_IF_SUPPORTED(
+        moduleEntries.Entry(
+            ModuleEntryPoint::Core_Initialize,
+            []() {}
+        ),
+        ""
+    );
+
+    moduleEntries.Call(ModuleEntryPoint::Core_Initialize);
+    EXPECT_EQ(name, "Core_Initialize");
+}
